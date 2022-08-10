2824d384801e850cbf28fd2b499bd5f7c5aca686
PaulOkoh <pauldokoh@gmail.com>
2022-08-09 21:02:29 -0500

    add showWinner function

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function getComputerChoice() {
-  function computerChoice() {
-    return Math.floor(Math.random()*3)+1
-  }
-  if(computerChoice() === 1) {
+    let randomNum =Math.floor(Math.random()*3)+1
+  if(randomNum === 1) {
     return "rock";
-  }else if(computerChoice() === 2) {
+  }else if(randomNum === 2) {
     return "paper"
   }else {
     return "scissors"
